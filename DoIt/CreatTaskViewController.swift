1fc4cf3201a95fae5bb0d1b8ed086535c61418e4
Chin Zhe Ying <chinzheying@gmail.com>
2017-09-25 21:35:46 +0800

    -Designed layout for the second screen -New UIViewController file for 2nd screen

---
+++ DoIt/CreatTaskViewController.swift
@@ -0,0 +1,28 @@
+ //
+//  CreatTaskViewController.swift
+//  DoIt
+//
+//  Created by Chin Zhe Ying on Sep/25/17.
+//  Copyright © 2017 Chin Zhe Ying. All rights reserved.
+//
+
+import UIKit
+
+class CreatTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+    
+
+}
