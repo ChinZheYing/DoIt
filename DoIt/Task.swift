9253d9e758b11d51e60d3e412491ec06c1e388d7
Chin Zhe Ying <chinzheying@gmail.com>
2017-09-25 21:01:26 +0800

    -Put data in TableView -Add exclamation mark

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Chin Zhe Ying on Sep/25/17.
+//  Copyright © 2017 Chin Zhe Ying. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
