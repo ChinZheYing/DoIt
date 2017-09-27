09a40446a616a7ad17cc9f38118db4b18d35005f
Chin Zhe Ying <chinzheying@gmail.com>
2017-09-27 22:41:41 +0800

    Added core data bug fix

--- DoIt/CompleteTaskViewController.swift
+++ DoIt/CompleteTaskViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  CompleteTaskViewController.swift
 //  DoIt
 //
@@ -10,28 +10,31 @@ import UIKit
 
 class CompleteTaskViewController: UIViewController {
 
-    var task = Task()
+    @IBOutlet weak var taskLabel: UILabel!
+    var task : Task? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        if task!.important {
+            taskLabel.text = "❗️\(task!.name!)"
+        } else{
+            taskLabel.text = task!.name!
+         }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func completeTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        context.delete(task!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
     }
-    */
+    
+    
 
 }
